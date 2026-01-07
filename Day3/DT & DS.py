6f3c54f8822306ace94a56e40a10a70972820483
patel harsh s <harshspatel1250@gmail.com>
2026-01-07 11:41:40 +0530

    Add files via upload

---
+++ Day3/DT & DS.py
@@ -0,0 +1,38 @@
+Python 3.13.9 | packaged by Anaconda, Inc. | (main, Oct 21 2025, 19:11:29) [Clang 20.1.8 ] on darwin
+Enter "help" below or click "Help" above for more information.
+>>> #Create an integer variable and a float variable, and add them together. Check output type
+>>> a=25
+>>> b=5.5
+>>> result = a + b
+>>> result
+30.5
+>>> type(result)
+<class 'float'>
+>>> a=45
+>>> b=3.8
+>>> result = a + b
+>>> result
+48.8
+>>> type(result)
+<class 'float'>
+>>> name = "harsh"
+>>> color = "Navy Blue"
+>>> result = name + " fvt color is " + color
+>>> result
+'harsh fvt color is Navy Blue'
+>>> a="10"
+>>> b=int(a)
+>>> print(b)
+10
+>>> #4 Create a list containing your favorite books
+>>> fvt_book = ['Atomic Habit','Zero to One','ikigai']
+>>> fvt_book
+['Atomic Habit', 'Zero to One', 'ikigai']
+>>> #5 Create a tuple containing the numbers 1, 3, and 5
+>>> numbers = (1,3,5)
+>>> numbers
+(1, 3, 5)
+>>> #6 Create a dictionary containing the key-value pairs for your name, age, and hometown
+>>> detail = {"name": "Harsh", "age": 21, "hometown": "Kapadvanj"}
+>>> detail
+{'name': 'Harsh', 'age': 21, 'hometown': 'Kapadvanj'}
